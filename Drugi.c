81628294e70588261ac541db0a8829eb3d60b9aa
sanja-susnjar <73340000+sanja-susnjar@users.noreply.github.com>
2020-11-08 16:02:47 +0100

    Update Drugi.c

--- Drugi.c
+++ Drugi.c
@@ -1,9 +1,10 @@
-#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS	
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 typedef struct osoba* Pozicija;
+
 typedef struct osoba {
 	char ime[20];
 	char prezime[20];
@@ -11,123 +12,133 @@ typedef struct osoba {
 	Pozicija next;
 }Osoba;
 
-int unosP(Pozicija);
-int Ispis(Pozicija);
-int unosK(Pozicija);
-Pozicija Trazi(Pozicija);
-Pozicija TraziPreth(char* , Pozicija );
-int Brisanje(char*, Pozicija);
-int Unos(Pozicija);
-
+int dodajP(Pozicija);//dodavanje na pocetak
+Pozicija dodajO(void);//dodavanje novog elementa
+int dodajK(Pozicija); //dodavanje elementa na kraj
+int ispis(Pozicija);
+Pozicija traziP(Pozicija);//trazenje po prezimenu
+int brisanjeIzListe(Pozicija);
+Pozicija traziPrethodni(Pozicija,Pozicija);
 int main() {
 	Osoba head;
 	head.next = NULL;
-	char prezime[20];
 
-	unosP(&head);
-	Ispis(head.next);
-	unosK(&head);
-	Trazi(head.next);
-	printf("\nPrezime za brisanje? ");
-	scanf("%s", prezime);
-	Brisanje(prezime, &head);
+	int odabir;
+	do {
+		printf("\n\nOdaberite opciju: ");
+		printf("\n1. Unos elementa na pocetak liste");
+		printf("\n2.Ispis liste ");
+		printf("\n3.Unos elementa na kraj liste");
+		printf("\n4.Pronadi element(po prezimenu)");
+		printf("\n5.Brisanje elementa iz liste");
+		printf("\n0 za izlaz");
+		printf("\n");
+		scanf(" %d", &odabir);
+
+		switch (odabir)
+		{
+		case 1:
+			dodajP(&head);
+			break;
+		case 2:
+			ispis(head.next);
+			break;
+		case 3:
+			dodajK(&head);
+			break;
+		case 4:
+			traziP(head.next);
+			break;
+		case 5:
+			brisanjeIzListe(&head);
+			break;
+
+		}
+	} while (odabir != 0);
+	return 0;
+}
+int dodajP(Pozicija P) {
+	Pozicija q;
+	q = (Pozicija)malloc(sizeof(Osoba));
+	q=dodajO();
+	q->next = P->next;
+	P->next = q;
 
 	return 0;
 }
 
-int unosP(Pozicija P) {
-	Pozicija a;
-	a = (Pozicija)malloc(sizeof(Osoba));
-	Unos(a);
-	a->next = P->next;
-	P->next = a;
+Pozicija dodajO(void) {
+	Pozicija P;
+	P = (Pozicija)malloc(sizeof(Osoba));
 
+	printf("Unesite ime: ");
+	scanf("%s", P->ime);
+	printf("Unesite prezime: ");
+	scanf("%s", P->prezime);
+	printf("Unesite godinu rodenja: ");
+	scanf("%d", &P->godina);
+	return P;
 }
 
-int Ispis(Pozicija P) {
-	while (P != NULL) {
-		printf("\nIme: %s\n", P->ime);
-		printf("\nPrezime: %s", P->prezime);
-		printf("\nGodina: %d", P->godina);
-		P = P->next;
+int ispis(Pozicija P) {
+
+	if (P == NULL) {
+		printf("Lista je prazna");
 	}
 
-}
+	
+	while (P != NULL) {
+		printf("\n %s %s %d", P->ime, P->prezime, P->godina);
+		P =  P->next;
+	}
 
-int unosK(Pozicija P) {
+	return 0;
+}
+int dodajK(Pozicija P) {
 	Pozicija a;
 	a = (Pozicija)malloc(sizeof(Osoba));
-	Unos(a);
+	a = dodajO();
+	if (P == NULL) {
+		printf("Lista je prazna");
+	}
+
 	while (P->next != NULL) {
 		P = P->next;
 	}
 	a->next = P->next;
 	P->next = a;
-}
-
-
-
-int Unos(Pozicija P) {
-	printf("\nIme? ");
-	scanf("%s", P->ime);
-	printf("\nPrezime? ");
-	scanf("%s", P->prezime);
-	printf("\nGodina rodenja? ");
-	scanf("%d", &P->godina);
-
 	return 0;
 }
 
-Pozicija Trazi(Pozicija P)
-{
+Pozicija traziP(Pozicija P) {
+	
 	char prezime[20];
-	printf("Unesite prezime koje zelite pretraziti: ");
-	scanf("%s", prezime);
-	Pozicija x = NULL;
-	while (P != NULL)
-	{
-		if (strcmp(P->prezime, prezime) == 0)
-		{
-			printf("Pronadena osoba s prezimenom %s\n", prezime);
-			printf("ime osobe: %s\n", P->ime);
-			printf("prezime osobe: %s\n", P->prezime);
-			printf("godina rodenja osobe: %d\n", P->godina);
-			x = P;
-		}
-		P = P->next;
-	}
-	if (x == NULL)
-	{
-		printf("osoba s %s prezimenom ne postoji u listi.\n", prezime);
-	}
-}
-Pozicija TraziPreth(char* prezime, Pozicija P)
-{
-	Pozicija prev = P;
-	P = P->next;
-	while (P != NULL && strcmp(P->prezime, prezime))
-	{
-		prev = P;
-		P = P->next;
-	}
-	if (P == NULL)
-	{
-		printf("Element ne postoji u listi. ");
-		return NULL;
-	}
-	return prev;
-}
-int Brisanje(char* prezime, Pozicija P)
-{
-	Pozicija prev;
-	prev = TraziPreth(prezime, P);
-	if (prev != NULL)
-	{
-		P = prev->next;
-		prev->next = P->next;
-		free(P);
-	}
 
+	printf("Koje prezime zelite pretraziti?\n");
+	scanf("%s", prezime);
+
+	while (P != NULL && strcmp(P->prezime, prezime)) {
+		P = P->next;
+	}
+	printf("Osoba s trazenim prezimenom je %s %s %d",P->ime, P->prezime, P->godina);
+
+	return P;
+
+}
+int brisanjeIzListe(Pozicija P) {
+	char prezime[20];
+	Pozicija pom,prev;
+	
+	pom = traziP(P);
+	prev = traziPrethodni(P, pom);
+	prev->next = pom->next;
+	free(pom);
 	return 0;
 }
 
+Pozicija traziPrethodni(Pozicija P, Pozicija pom) {
+	while (P->next != pom) {
+		P = P->next;
+	}
+	return P;
+}
